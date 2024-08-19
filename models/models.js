c5fe301995a2e02ab5fa3365ea67ffde1acc154b
o <3vefaaaa@mail.ru>
2024-08-19 20:35:09 +0700

    h

--- models/models.js
+++ models/models.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const Point = new mongoose.Schema({
-    name: {type: String, required: true},
-    type: {type: String, required: true},
-    coordinates: {type: Array, required: true},
-    street: {type: String, required: true},
-    description: {type: String, required: true},
-    accessibility: {type: Array, required: true},
+    name: {type: String, required: false},
+    type: {type: String, required: false},
+    coordinates: {type: Array, required: false},
+    street: {type: String, required: false},
+    description: {type: String, required: false},
+    accessibility: {type: Array, required: false},
     photos: {type: Array}
 })
 
