32438bee41ce139bc55f9764ac9a85d74d12a866
o <3vefaaaa@mail.ru>
2024-08-31 19:08:31 +0700

    some cors settings

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const router = require('./routes/index')
 const PORT = process.env.PORT || 5000
 
 const app = express()
-app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
