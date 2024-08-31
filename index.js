1c942f30482c444f38bc5662a79a65c2cbe9da6a
o <3vefaaaa@mail.ru>
2024-08-31 22:00:47 +0700

    cors

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 require('dotenv').config()
-const  mongoose =  require('mongoose')
+const mongoose = require('mongoose')
 const express = require("express");
-const cors = require('cors')
+const cors = require('cors') // Required for CORS configuration
 const path = require("path");
 const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
 const PORT = process.env.PORT || 5000
 
 const app = express()
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+
+// **CORS configuration:**
+app.use(cors({
+  origin: 'https://kdm-map.ru', // Replace with your client-side origin
+  credentials: true // Enable cookies for cross-origin requests (if needed)
+}));
+
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 app.use('/api', router)
 
 const start = async () => {
-    try {
-        await mongoose.connect(process.env.DB_URL)
-        app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT))
-    } catch (e) {
-        console.log(e)
-    }
+  try {
+    await mongoose.connect(process.env.DB_URL)
+    app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT))
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 start()
